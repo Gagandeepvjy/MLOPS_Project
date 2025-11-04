17f67ab8ae9630d08404620b9b7416279cadfbb1
Gagandeep <gagandeepvjy41003@gmail.com>
2025-11-04 12:48:07 +0530

    project structure,venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Gagandeep",
+    author_email="gagandeep41003@gmail.com",
+    packages=find_packages()
+)
